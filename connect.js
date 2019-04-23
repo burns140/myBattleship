0d034f1d007cf0acfc78f1a360629c1739defb9f
burns140 <burns140@purdue.edu>
2019-04-22 23:43:03 -0400

    works across multiple users. need to add checks so only half of screen updates

--- connect.js
+++ connect.js
@@ -1,6 +1,8 @@
-var http = require('http');
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-http.createServer(function (req, res) {
+/**http.createServer(function (req, res) {
 	//res.end('Hello World');
 	var min = 1;
 	var max = 1000000;
@@ -8,4 +10,23 @@ http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	//res.write('<canvas id=\"myCanvas\" width=\"10000\" height=\"10000\"></canvas>');
 	res.end('Random Number Generated: ' + random.toString());
-}).listen(3000);
+}).listen(3000);**/
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/testgrid.html');
+});
+
+io.on('connection', function(socket) {
+	console.log('user connected');
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
+	socket.on('clicked', function(msg) {
+		console.log('somebody shot something');
+		io.emit('clicked', msg);
+	});
+});
+
+http.listen(3000, function() {
+	console.log('listening on 3000');
+});
