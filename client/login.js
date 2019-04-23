30e4aaa251a84df5e9a9d94ecbe8f42e9f093f20
dhairyaa <amind@purdue.edu>
2019-04-23 16:05:32 -0400

    Fixed adding to database and added menu page

--- client/login.js
+++ client/login.js
@@ -1,54 +1,49 @@
-var socket = io();
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB38Y5PiCyAoA1jTttFJbN0HliwjkstPvo",
+  authDomain: "battleship-92ba9.firebaseapp.com",
+  databaseURL: "https://battleship-92ba9.firebaseio.com",
+  projectId: "battleship-92ba9",
+  storageBucket: "battleship-92ba9.appspot.com",
+  messagingSenderId: "67219704226"
+};
+firebase.initializeApp(config);
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBD-0alEnH2GSG4-ap3JiGhDHi0M5MFhSg",
-    authDomain: "event-planner-2bed5.firebaseapp.com",
-    databaseURL: "https://event-planner-2bed5.firebaseio.com",
-    projectId: "event-planner-2bed5",
-    storageBucket: "event-planner-2bed5.appspot.com",
-    messagingSenderId: "654317062394"
-  };
-  firebase.initializeApp(config);
+var database = firebase.database();
+var prevFunc = "temp";
 
-firebase.auth().onAuthStateChanged(function(user) {
-  
-  if(user){
-    // User is signed in
-
-    var user = firebase.auth().currentUser;
-
-    if(user != null){
-      
-      var email_id = user.email;
-      document.getElementById("login-title").innerHTML = "Welcome User: " + email_id;
-
+firebase.auth().onAuthStateChanged(function(user){
+    if(user){
+      if(prevFunc == "signup"){
+        try{
+          createNewUser();
+        }catch(e){
+          window.alert(e.message);
+        }
+      }
+      // User is logged in
+      window.location.href = "menu";
     }
-  }
-  else{
-    // User is not signed in
-    document.getElementById("signup-form").addEventListener("submit",signup,false);
-    document.getElementById("signin-form").addEventListener("submit",login,false);
-    
-  }
 });
-
-
 
 function login(){
 
-  var userEmail = document.getElementById("email_field").value;
-  var userPass = document.getElementById("password_field").value;
+  prevFunc = "login";
 
-  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
+  var userEmail = document.getElementById("get_email").value;
+  var userPass = document.getElementById("get_pass").value;
 
-    window.alert("Error : " + errorMessage);
-
-    // ...
-  });
+  if(!userEmail || !userPass){
+    window.alert("email and password can't be empty");
+  }
+  else{
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert("Error : " + errorCode + "  " + errorMessage);
+    });
+  }
 
 }
 
@@ -56,18 +51,45 @@ function signup(){
   var userEmail = document.getElementById("get_signup_email").value;
   var userPass = document.getElementById("get_signup_pass").value;
 
-  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
+  if(!userEmail || !userPass){
+    window.alert("email and password can't be empty");
+  }
+  else{
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPass)
+    .then(prevFunc = "signup")
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert("Error : " + errorCode + "  " + errorMessage);
+    });
+  }
+}
 
-    window.alert("Error : " + errorMessage);
+function createNewUser(){
 
-    // ...
+  var rootRef = firebase.database().ref().child('users');
+  var userRef = rootRef.child(firebase.auth().currentUser.uid);
+
+  userRef.set({
+    uid: firebase.auth().currentUser.uid,
+    email: firebase.auth().currentUser.email,
+    wins: 0,
+    played: 0
+  }, function(error) {
+    if (error) {
+      // The write failed...
+    } else {
+      // Data saved successfully!
+    }
   });
-
 }
 
 function logout(){
   firebase.auth().signOut();
+  window.location.href = "/";
+}
+
+if(window.closed){
+  logout();
 }
--- server.js
+++ server.js
@@ -1,107 +1,53 @@
+// Setting port
 var PORT = process.env.PORT || 3000;
 
+var firebase = require('firebase');
 var express = require('express');
 var app = express();
-
-var http = require('http');
-
-const server = http.Server(app);
+var path = require('path');
 const bodyParser = require("body-parser");
-var firebase = require("firebase");
-require('firebase/auth');
-require('firebase/database');
 
-// Initialize Firebase
-firebase.initializeApp({
-  apiKey: "AIzaSyB38Y5PiCyAoA1jTttFJbN0HliwjkstPvo",
-  authDomain: "battleship-92ba9.firebaseapp.com",
-  databaseURL: "https://battleship-92ba9.firebaseio.com",
-  projectId: "battleship-92ba9",
-  storageBucket: "battleship-92ba9.appspot.com",
-  messagingSenderId: "67219704226"
-});
-
-var auth = firebase.auth();
-var db = firebase.database();
-
+// Allow static files from client directory to run
 app.use(express.static('client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-
-// Respond with login page if user is not logged in
+// Respond with login page
 app.get('/', function(req, res){
-  auth.onAuthStateChanged(function(user){
-    if(user){
-      // User is signed in
-      var user = firebase.auth().currentUser;
-      var email_id = user.email;
-      document.getElementById("login-title").innerHTML = "Welcome User: " + email_id;
-      
-      console.log('user is already logged in')
-    }
-    else{
-      // User is not signed in
-      res.sendFile(__dirname + '/client/login.html');
-      console.log('login page sent');
-    }
-  })
+  // Send the login page
+  res.sendFile(path.join(__dirname,'client','login.html'));
+  console.log('login page sent');
 });
 
-app.post('/login', function(req, res){
-  var email = req.body.email;
-  var password = req.body.password;
-
-  auth.setPersistence(firebase.auth.Auth.Persistence.SESSION).then(function() {
-    auth.signInWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-  
-      window.alert("Error : " + errorMessage);
-  
-      // ...
-    });
-  }).catch(function(error){
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-
-    window.alert("Error : " + errorMessage);
-
-    // ...
-  })
-  console.log('login post req');
+// Respond with login page
+app.get('/login', function(req, res){
+  // Send the login page
+  res.sendFile(path.join(__dirname,'client','login.html'));
+  console.log('login page sent');
 });
+
 
 // Respond with signup page
 app.get('/signup', function(req, res){
-  res.sendFile(__dirname + '/client/signup.html')
+  res.sendFile(path.join(__dirname,'client','signup.html'));
   console.log('signup page sent')
 });
 
-// Sign up user
-app.post('/signup', function(req, res){
-  var email = req.body.email;
-  var password = req.body.password;
-
-  auth.createUserWithEmailAndPassword(email, password).catch(function(error){
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-
-    window.alert("Error : " + errorMessage);
-
-    // ...
-  });
-
-  console.log('signup post req');
+// Respond with menu page
+app.get('/menu', function(req, res){
+  res.sendFile(path.join(__dirname,'client','menu.html'));
+  console.log('menu page sent')
 });
 
+// Respond with menu page
+app.get('/play', function(req, res){
+  res.sendFile(path.join(__dirname,'testgrid.html'));
+  console.log('testgrid page sent')
+});
+
+// Listen for requests at PORT
 app.listen(PORT, function() {
-  console.log('Battleship server running on port '+PORT);
+  console.log('Battleship server running on port : '+ PORT);
 });
-
-var io = require('socket.io')(server);
